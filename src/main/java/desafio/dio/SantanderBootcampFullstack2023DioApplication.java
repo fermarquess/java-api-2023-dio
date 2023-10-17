c40999ae9a125baf26b072cd48d8d82e5c58f971
Fernanda Marques <fermarquessantos@gmail.com>
2023-10-17 16:25:00 -0300

    Initial commit

---
+++ src/main/java/desafio/dio/SantanderBootcampFullstack2023DioApplication.java
@@ -0,0 +1,13 @@
+package desafio.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SantanderBootcampFullstack2023DioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SantanderBootcampFullstack2023DioApplication.class, args);
+	}
+
+}
